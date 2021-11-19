8e3a0177cff2ab4c0513788af93c9ed63ff1b89f
PaivaH <henio_paiva@hotmail.com>
2021-11-19 20:20:59 +0000

    deploy para aws

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,5 @@
 module.exports = {
     devServer: {
         disableHostCheck: true
-    },
-    pwa: {
-        name: 'Rota de fuga',
-        themeColor: '#4DBA87',
-        msTileColor: '#000000',
-        start_url: '/',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-    
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-          // swSrc is required in InjectManifest mode.
-          swSrc: 'dev/sw.js',
-          // ...other Workbox options...
-        }
     }
 };
